7ab2722968c12152efc3892bf29d2da6439dead3
mycs95 <samsonmolou@gmail.com>
2019-01-20 18:29:51 +0000

    update member search suggestion

--- ccis_blocs/lib/src/blocs/members/members_list_bloc.dart
+++ ccis_blocs/lib/src/blocs/members/members_list_bloc.dart
@@ -10,9 +10,11 @@ class MembersListBloc {
   final Sink<Member> addMember;
   final Sink<String> deleteMember;
   final Sink<Member> updateMember;
+  final Sink<String> searchMember;
 
   // Outputs
   final Stream<List<Member>> members;
+  final Stream<List<Member>> searchMemberResult;
 
   // Cleanup
   final List<StreamSubscription<dynamic>> _subscriptions;
@@ -27,7 +29,7 @@ class MembersListBloc {
     final addMemberController = StreamController<Member>(sync: true);
     final deleteMemberController = StreamController<String>(sync: true);
     final updateMemberController = StreamController<Member>(sync: true);
-
+    final searchMemberController = BehaviorSubject<String>(sync: true);
     // In some cases, we need to simply route user interactions to our data
     // layer. In this case, we'll listen to the streams. In order to clean
     // these subscriptions up, we'll pop them in a list and ensure we cancel
@@ -36,13 +38,32 @@ class MembersListBloc {
       // When a user adds an item, add it to the repository
       addMemberController.stream.listen(interactor.addNewMember),
       deleteMemberController.stream.listen(interactor.deleteMember),
-      updateMemberController.stream.listen(interactor.updateMember)
+      updateMemberController.stream.listen(interactor.updateMember),
+
     ];
+
+    // To calculate the result of search, we combine the members with the current
+    // search key and return the result of the search.
+    //
+    // Every time the members or the search query changes the visible items will emit
+    // once again. We also convert the normal Stream into a BehaviorSubject
+    // so the Stream can be listened to multiple times
+    final searchMemberResultController = BehaviorSubject<List<Member>>();
+
+    Observable
+      .combineLatest2<List<Member>, String, List<Member>>(
+      interactor.members,
+      searchMemberController.stream,
+      _searchMembers,
+    )
+      .pipe(searchMemberResultController);
 
     return MembersListBloc._(
       addMemberController,
       deleteMemberController,
       updateMemberController,
+      searchMemberController,
+      searchMemberResultController,
       interactor.members,
       subscriptions,
     );
@@ -52,8 +73,24 @@ class MembersListBloc {
       this.addMember,
       this.deleteMember,
       this.updateMember,
+      this.searchMember,
+      this.searchMemberResult,
       this.members,
       this._subscriptions);
+
+  static List<Member> _searchMembers(List<Member> members, String query) {
+    final Iterable<Member> suggestions = members.where(
+        (member) => member.firstName.contains(query)
+            || member.secondName.contains(query)
+            || member.study.contains(query)
+            || member.community.contains(query)
+            || member.residence.contains(query)
+            || member.bedroomNumber.contains(query)
+            || member.phoneNumber.contains(query)
+    );
+
+    return suggestions.toList();
+  }
 
   // This method should close down all sinks and cancel all stream
   // subscriptions. This ensures we free up resources and don't trigger odd
@@ -62,6 +99,7 @@ class MembersListBloc {
     addMember.close();
     deleteMember.close();
     updateMember.close();
+    searchMember.close();
     _subscriptions.forEach((subscriptions) => subscriptions.cancel());
   }
 
